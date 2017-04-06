37492886ea823c377237dfa2547b51752e8093e4
ikastaroa <ikastaroa@TOLOSALDEA.COM>
2017-04-06 12:49:34 +0200

    selector populatzen errorea

---
+++ PHP/kodeak.php
@@ -0,0 +1,30 @@
+<?php
+/* user + pass jaso*/
+$user = $_GET['usuario'];
+
+session_start();
+$servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
+$usuario = "u199017461_sgta";//root u199017461_sgta
+$password = "n1fXhn9qyo";//n1fXhn9qyo
+$sdb = "mantentzeDB";//u199017461_sgta
+
+//$mysqli =new mysqli ($servidor,$usuario,$password, $sdb);
+$mysqli =new mysqli ("localhost","root","", $sdb);
+if ($mysqli->connect_error) {
+    printf("Connection failed: " . $mysqli->connect_error);
+} 
+
+$erantzuna = array(); 
+
+		$erab = $mysqli->query( "SELECT izena FROM akatskodea" );
+		$num_rows=mysqli_num_rows($erab);
+		echo $num_rows;
+		while($datos=mysqli_fetch_array($erab,MYSQLI_ASSOC)){
+			$erantzuna[]=array_map('utf8_encode', $datos);
+		}
+
+/*emaitza json formatura bihurtzen da*/
+$resultadosJson = json_encode($erantzuna);
+/* emaitza erakusten da, nabigatzaileetan errorerik ez emateko moduan */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+?>
