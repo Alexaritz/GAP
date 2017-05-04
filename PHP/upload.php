6474837a153773e6fb993c7fdf7fe426d203e240
ikastaroa <ikastaroa@TOLOSALDEA.COM>
2017-05-04 12:57:57 +0200

    INPL:png+jpg+gif iyo, errore mezuak json erabiliz, argazkian errorea abisua ez da bidaltzen

--- PHP/upload.php
+++ PHP/upload.php
@@ -7,66 +7,73 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+		header('Content-Type: application/json');
+		echo json_encode(['erantzuna' => "File is an image - " . $check["mime"] . ".", 'ok' => "errorea"]);
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+		header('Content-Type: application/json');
+		echo json_encode(['erantzuna' => "File is not an image.", 'ok' => "errorea"]);
         $uploadOk = 0;
     }
-}
-/* Check if file already exists
+}/*
+ //Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }*/
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
+	header('Content-Type: application/json');
+	echo json_encode(['erantzuna' => "Sorry, your file is too large.", 'ok' => "errorea"]);
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	header('Content-Type: application/json');
+	echo json_encode(['erantzuna' => "Sorry, only JPG, JPEG, PNG & GIF files are allowed.", 'ok' => "errorea"]);
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+
 } else {
-	
-//IZENA LORTU
-					session_start();
-					$servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
-					$usuario = "u199017461_sgta";//root u199017461_sgta
-					$password = "n1fXhn9qyo";//n1fXhn9qyo
-					$sdb = "mantentzeDB";//u199017461_sgta
+	// if everything is ok, try to upload file
+	//IZENA LORTU
+	session_start();
+	$servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
+	$usuario = "u199017461_sgta";//root u199017461_sgta
+	$password = "n1fXhn9qyo";//n1fXhn9qyo
+	$sdb = "mantentzeDB";//u199017461_sgta
 
-					//$mysqli =new mysqli ($servidor,$usuario,$password, $sdb);
-					$mysqli =new mysqli ("localhost","root","", $sdb);
-					if ($mysqli->connect_error) {
-						printf("Connection failed: " . $mysqli->connect_error);
-					} 
+	//$mysqli =new mysqli ($servidor,$usuario,$password, $sdb);
+	$mysqli =new mysqli ("localhost","root","", $sdb);
+	if ($mysqli->connect_error) {
+		printf("Connection failed: " . $mysqli->connect_error);
+		header('Content-Type: application/json');
+		echo json_encode(['erantzuna' => "Konexioan errorea."]); 
+	} 
 
-						$erab = $mysqli->query( "SELECT * FROM lanagindua where  argazkia!=''" );
-						$num_rows=mysqli_num_rows($erab);
-						$num_rows=$num_rows-1;
-					//IRUDI KONPRIMITUA IZEN BERRIKIN GORDE
-					$target_file2= $target_dir . $num_rows;
-					//$target_file3=
-					$image = imagecreatefromjpeg($_FILES["fileToUpload"]["tmp_name"]);
-					imagejpeg($image, $target_file2 . "argazkia.jpeg", 75);
-					echo "Argazkia zuzen igo da.";
-					/*$erantzuna = array(); 
-					$erantzuna["mezua"] = "Datu zuzenak.";
-					$resultadosJson = json_encode($erantzuna);*/
-    /*if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		
-					
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }*/
+		$erab = $mysqli->query( "SELECT * FROM lanagindua where  argazkia!=''" );
+		$num_rows=mysqli_num_rows($erab);
+		$num_rows=$num_rows;
+		//IRUDI KONPRIMITUA IZEN BERRIKIN GORDE
+		$target_file2= $target_dir . $num_rows;
+		if($imageFileType == "jpg" || $imageFileType == "jpeg"){
+			$image = imagecreatefromjpeg($_FILES["fileToUpload"]["tmp_name"]);
+		}
+		if($imageFileType == "png"){
+			$image = imagecreatefrompng($_FILES["fileToUpload"]["tmp_name"]);
+		}
+		if($imageFileType == "gif"){
+			$image = imagecreatefromgif($_FILES["fileToUpload"]["tmp_name"]);
+		}	
+		if (imagejpeg($image, $target_file2 . "argazkia.jpeg", 75)){
+			header('Content-Type: application/json');
+			echo json_encode(['erantzuna' => "Argazkia erabat igo da.", 'ok' => "zuzena"]);
+		}else{
+			header('Content-Type: application/json');
+			echo json_encode(['erantzuna' => "Argazkia igotzean errorea.", 'ok' => "okerra"]);
+		}
 }
 ?>
