dcc81c51bcf6e0be61bf380755b0ce53ad8d2257
ikastaroa <ikastaroa@TOLOSALDEA.COM>
2017-05-11 12:01:52 +0200

    INPL:abisu egoera zuzen agertu, abisua osatzeko datuak/nola osatu diseinua falta;DB:Datuak sartu

--- PHP/egoeraLortu.php
+++ PHP/egoeraLortu.php
@@ -1,7 +1,7 @@
 <?php
 /* user + pass jaso*/
 $user = $_GET['usuario'];
-
+$id = $_GET['lanid'];
 //session_start();
 $servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
 $usuario = "u199017461_sgta";//root u199017461_sgta
@@ -22,7 +22,9 @@ $erantzuna = array();
 			$erantzuna[$int]=array_map('utf8_encode', $datos);
 			$int=$int+1;
 		}
-		$erantzuna[$int]="A";
+		$erab = $mysqli->query( "SELECT * FROM lanAgindua where id='$id'" );
+		$row = mysqli_fetch_array($erab, MYSQLI_ASSOC);
+		$erantzuna[$int]=$row["egoera"];
 		$resultadosJson=json_encode( $erantzuna );
 /*emaitza json formatura bihurtzen da*/
 /* emaitza erakusten da, nabigatzaileetan errorerik ez emateko moduan */
