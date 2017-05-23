501bffb60c751ad3004cc1d0b76152cb1834e04a
ikastaroa <ikastaroa@TOLOSALDEA.COM>
2017-05-23 13:09:42 +0200

    INPL:langileOrdua ezabatu egin; lanXehetasunak json .fail zuzendu behar;

--- PHP/ezabatuLangileOrdua.php
+++ PHP/ezabatuLangileOrdua.php
@@ -1,8 +1,8 @@
 <?php
-/*datuak jaso*/
+/* user + pass jaso*/
 $user = $_GET['usuario'];
-$lanid = $_GET['lanid'];
-$egoera = $_GET['egoera'];
+$id = $_GET['id'];
+
 session_start();
 $servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
 $usuario = "u199017461_sgta";//root u199017461_sgta
@@ -14,18 +14,30 @@ $mysqli =new mysqli ("localhost","root","", $sdb);
 if ($mysqli->connect_error) {
     printf("Connection failed: " . $mysqli->connect_error);
 } 
-//if(isset($_POST["submit"]) &&) {
+
 $erantzuna = array(); 
-	if ($lanid!=""){
-			$erab = $mysqli->query( "UPDATE lanagindua SET egoera='$egoera' WHERE id='$lanid' AND egoera!='$egoera'");
-			$erantzuna["mezua"] = "Egoera eguneratu da.";
-	}else{
-		$erantzuna["mezua"] = "Errorea egoera eguneratzean.";
-	}
+
+		//if($mysqli->query( "DELETE * FROM langileorduak WHERE id='$id'" )===TRUE{
+			$mysqli->query( "DELETE FROM langileorduak WHERE id=$id" );
+			$erantzuna["zuzen"] = "ok";
+			$erantzuna["mezua"] = "Zuzen ezabatu da.";
+		/*}else{
+			$erantzuna["zuzen"] = "error";
+			$erantzuna["mezua"] = "Errorea gertatu da ezabaketan.";
+		}*/
+		/*$num_rows=mysqli_num_rows($erab);
+		if ($num_rows> 0){
+			$_SESSION['logged'] = true;
+			$_SESSION['username'] = $user;
+			$erantzuna["mezua"] = "Datu zuzenak.";
+			$erantzuna["onarpena"] = "ok";
+		}else{
+			$erantzuna["mezua"] = "Erabiltzaile eta pasahitz okerrak.";
+			$erantzuna["onarpena"] = "error";
+		}*/
 
 /*emaitza json formatura bihurtzen da*/
 $resultadosJson = json_encode($erantzuna);
 /* emaitza erakusten da, nabigatzaileetan errorerik ez emateko moduan */
 echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
-//$conn->close();
 ?>
--- PHP/lanaOsatu.php
+++ PHP/lanaOsatu.php
@@ -21,12 +21,15 @@ $erantzuna = array();
 	if ($user!="" && $lanid!=""){
 			$erab = $mysqli->query( "INSERT INTO lanazalpena (lanid, arduraduna, azalpena, materiala, itxiData) 
 			VALUES('$lanid', '$user','$azalpena','$materiala',now()) ON DUPLICATE KEY UPDATE    
-			azalpena='$azalpena', materiala='$materiala', itxiData=now()" );
+			azalpena=IF(azalpena!='$azalpena', '$azalpena', azalpena), 
+			materiala=IF(materiala!='$materiala', '$materiala', materiala),
+			itxiData=IF(itxiData!=now(), now(), itxiData)" );
 			$erantzuna["mezua"] = "Zuzen txertatu da.";
 	}else{
 		$erantzuna["mezua"] = "Errorea txertatzean. Datu guztiak bete behar dira.";
 	}
-
+$erantzuna["mysql"] = mysqli_error($mysqli);
+//$erantzuna[] = "fsadgd";
 /*emaitza json formatura bihurtzen da*/
 $resultadosJson = json_encode($erantzuna);
 /* emaitza erakusten da, nabigatzaileetan errorerik ez emateko moduan */
