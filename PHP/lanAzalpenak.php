b75bee376abffb3381b2328bb3703fbc8d4f5e1e
ikastaroa <ikastaroa@TOLOSALDEA.COM>
2017-06-13 12:55:35 +0200

    INPL:column filter ta data range falta+ inner join taulak hasi+rowgroup jarri

--- PHP/lanAzalpenak.php
+++ PHP/lanAzalpenak.php
@@ -5,12 +5,12 @@ $user = $_GET['usuario'];
 $id = $_GET['lanid'];
 
 $erantzuna = array();
-		$erab = $mysqli->prepare( "SELECT * FROM lanazalpena" );
+		$erab = $mysqli->prepare( "SELECT lanazalpena.id, lanazalpena.lanid, lanazalpena.arduraduna, lanazalpena.azalpena, lanazalpena.materiala, lanazalpena.itxiData, lanagindua.saila, langileorduak.langilea FROM lanazalpena INNER JOIN lanagindua ON lanazalpena.lanid = lanagindua.id INNER JOIN langileorduak ON lanazalpena.lanid=langileorduak.lanID" );
 		//$erab->bind_param("si", $user, $id );
 		$erab->execute();
-		$erab->bind_result($id, $lanid, $arduraduna, $azalpena, $materiala, $itxiData);
+		$erab->bind_result($id, $lanid, $arduraduna, $azalpena, $materiala, $itxiData, $saila, $langilea);
 		while ($erab->fetch()) {
-			$erantzuna[] = array('id'=> $id, 'lanid'=> $lanid, 'arduraduna'=> $arduraduna, 'azalpena'=> $azalpena, 'materiala'=> $materiala, 'itxiData'=> $itxiData);
+			$erantzuna[] = array('id'=> $id, 'lanid'=> $lanid, 'arduraduna'=> $arduraduna, 'azalpena'=> $azalpena, 'materiala'=> $materiala, 'itxiData'=> $itxiData, 'saila'=> $saila, 'langilea'=> $langilea);
 		}
 		$resultadosJson=json_encode( $erantzuna );
 /*emaitza json formatura bihurtzen da*/
